141e09398d22974bbc5df58c6cfdc1f4505b1e83
Tim Waters <chippy2005@gmail.com>
2014-01-10 10:37:56 -0500

    Add validation to map layers join model to ensure that a map can not have multiple layers the same

--- app/models/map_layer.rb
+++ app/models/map_layer.rb
@@ -4,4 +4,6 @@ def self.table_name()
 end
 belongs_to :layer
 belongs_to :map, :foreign_key => "mapscan_id"
+validates_uniqueness_of :layer_id, :scope => :mapscan_id, :message => "Layer already has this map"
+
 end
