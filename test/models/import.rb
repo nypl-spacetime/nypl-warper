6f0df68e01a12e4f5cbb0cf1c84e5e1b99209da9
Tim Waters <chippy2005@gmail.com>
2015-05-22 15:25:10 +0100

    fix for moving fixtures out of the test firectories

--- test/models/import.rb
+++ test/models/import.rb
@@ -3,7 +3,7 @@ require 'vcr'
 
 
 VCR.configure do |config|
-  config.cassette_library_dir = "test/fixtures/vcr_cassettes"
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock 
 end
 
