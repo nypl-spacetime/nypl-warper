44aadc4d6b4b627ee248572914db2c4a0b2fb376
Tim Waters <chippy2005@gmail.com>
2015-06-25 16:31:59 +0100

    Add tile spec json file to push to s3 as well as tiles

--- app/models/concerns/tilestache.rb
+++ app/models/concerns/tilestache.rb
@@ -23,7 +23,7 @@ module Tilestache
       :access => key_id, 
       :bucket => bucket_name,
       :path => bucket_path
-      }
+    }
    
     config_json = tilestache_config_json(options)
     
@@ -52,7 +52,10 @@ module Tilestache
 
       return nil
     else
-       return true
+       
+      send_tile_config(options)
+      
+      return true
     end
     
     
@@ -86,22 +89,88 @@ module Tilestache
       }
     }
     
-#    test_config = {
-#      "cache" => {
-#        "name" => "Test",
-#        "path" => "/tmp/stache"
-#      },
-#      "layers" => {
-#        layer_name => {       
-#          "provider" => {
-#            "name" => "proxy", 
-#            "url" =>  url
-#          }
-#        }
-#      }
-#    }
+    #    test_config = {
+    #      "cache" => {
+    #        "name" => "Test",
+    #        "path" => "/tmp/stache"
+    #      },
+    #      "layers" => {
+    #        layer_name => {       
+    #          "provider" => {
+    #            "name" => "proxy", 
+    #            "url" =>  url
+    #          }
+    #        }
+    #      }
+    #    }
     
     JSON.pretty_generate(config)
   end
   
+  def send_tile_config(options)
+    bucket_name = options[:bucket]
+    access = options[:access]
+    secret = options[:secret]
+    path = options[:path]
+    
+    service = S3::Service.new(:access_key_id =>access, :secret_access_key => secret)
+    bucket = service.buckets.find(bucket_name)
+    
+    layer_name = options[:item_id].to_s
+    layer_name = "map-"+ layer_name if options[:item_type] == "map"
+    
+    tile_config_filename = "#{layer_name}spec.json"
+    tile_config_file = layer_name + "/" + tile_config_filename
+    tile_config_file = path + "/"+ tile_config_file unless path.blank?
+
+    the_json = tile_config_json(options)
+    
+    config_file = File.join(Rails.root, 'tmp', tile_config_filename)
+    File.open(config_file, "w+") do |f|
+      f.write(the_json)
+    end
+    
+    new_object = bucket.objects.build(tile_config_file)
+    new_object.content = open("tmp/#{tile_config_filename}")
+    new_object.save
+
+  end
+  
+  #config file to be sent to s3 as well
+  def tile_config_json(options)
+    layer_name = options[:item_id].to_s
+    layer_name = "map-"+ layer_name if options[:item_type] == "map"
+    
+    name = self.title if options[:item_type] == "map"
+    name = self.name if options[:item_type] == "layer"
+    
+    description  = self.description
+    
+    attribution ="From: <a href='http://digitalcollections.nypl.org/items/#{self.uuid}'>NYPL Digital Collections</a> | <a href='http://maps.nypl.org/warper/#{self.class.to_s.downcase}/#{self.id}/'>Warper</a> "
+    
+    bbox = self.bbox.split(",")
+   
+    tile_bbox = [bbox[0].to_f,bbox[1].to_f,bbox[2].to_f,bbox[3].to_f]
+  
+    centroid_y = tile_bbox[1] + ((tile_bbox[3] -  tile_bbox[1]) / 2)
+    centroid_x = tile_bbox[0] + ((tile_bbox[2] -  tile_bbox[0]) / 2)
+
+    config = {
+      "tilejson"      => "2.0.0",
+      "autoscale"   => true,
+      "name"        => "#{name}",
+      "description" => "#{description}",
+      "version"     => "1.5.0",
+      "attribution" => "#{attribution}",
+      "scheme"      => "xyz",
+      "tiles"       => ["http://maptiles.nypl.org/#{layer_name}/{z}/{x}/{y}.png"],
+      "minzoom"     => 1,
+      "maxzoom"     => APP_CONFIG['s3_tiles_max_zoom'],
+      "bounds"      => tile_bbox,
+      "center"      => [centroid_x, centroid_y, 18 ]
+    }
+    
+    return JSON.pretty_generate(config)
+
+  end
 end 
