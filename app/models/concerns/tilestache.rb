f6bbb7fb7689ad9e4454ab1f767a3d0f653de6d6
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-11-29 16:29:07 -0500

    :space_invader: max zoom as int in json spec

--- app/models/concerns/tilestache.rb
+++ app/models/concerns/tilestache.rb
@@ -187,7 +187,7 @@ module Tilestache
 
     name = self.title if options[:item_type] == "map"
     name = self.name if options[:item_type] == "layer"
-    max_zoom = options[:max_zoom].to_i || 21
+    max_zoom = options[:max_zoom].to_i || 21.to_i
 
     description  = self.description
 
