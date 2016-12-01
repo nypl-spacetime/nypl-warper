7714272eb0b53f71940c4a868620800517381594
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-12-01 15:56:01 -0500

    :space_invader: fixing bug on nil flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
   
   FLASH_NOTICE_KEYS = [:error, :notice, :warning]
   def flash_messages
-    return unless messages = flash.keys.select{|k| FLASH_NOTICE_KEYS.include?(k.to_sym)}
+    return unless messages = flash.select { |k, v| v.present? }.keys.select{|k| FLASH_NOTICE_KEYS.include?(k.to_sym)}
     formatted_messages = messages.map do |type|      
       content_tag :div, :id => type.to_s do
         message_for_item(flash[type], flash["#{type}_item".to_sym])
