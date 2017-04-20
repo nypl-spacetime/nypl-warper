8f4306b95176b6d45d0558ae689face234b269c2
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2017-04-20 11:51:03 -0400

    :space_invader: was missing the reimport route

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
       get 'inset_maps'
       post 'create_inset'
       post 'map_type'
+      post 'reimport'
       get 'export'
       get 'warp'
       get 'clip'
