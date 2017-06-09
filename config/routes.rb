91b94bde56449f0078a7ce6ed0ce3a3f588f86a0
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2017-06-09 16:39:45 -0400

    :space_invader: fixed reimport link controller with normal link no form

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
       get 'inset_maps'
       post 'create_inset'
       post 'map_type'
-      post 'reimport' => 'maps#reimport', :as =>'reimport_map'
+      post 'reimport'
       get 'export'
       get 'warp'
       get 'clip'
